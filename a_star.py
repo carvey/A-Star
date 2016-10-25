242f2f5cf6bae23cf2a56e115dde0136336b184b
carvey <j95.charles@gmail.com>
2016-10-24 21:20:35 -0400

    Updated some logic and documentation. Just started the process of integrating with networkx.

--- a_star.py
+++ a_star.py
@@ -8,6 +8,21 @@ Goal State:
 3 4 5
 6 7 8
 """
+import networkx as nx
+# import pylab
+
+# # Initialize the graph
+# graph = networkx.DiGraph()
+#
+# # Add edges
+# for edge in Main.__graph.edge_set:
+#     graph.add_edge(edge.node_from.name, edge.node_to.name)
+#
+# # Set layout, draw the graph, and display it
+# pos = networkx.shell_layout(graph)
+# networkx.draw(graph, pos, node_size=1500, node_color='yellow', edge_color='red', with_labels=True)
+# # Show the graph
+# # pylab.show()
 
 
 coord_map = {
@@ -22,11 +37,13 @@ coord_map = {
     8: (2,-2)
 }
 
+
 class Node:
 
     def __init__(self, val, pos):
         """
-        :param val: Node number
+        :param int val: Node number
+        :param int pos: Position
         """
         self.val = val # actual value
         self.pos = pos # puzzle location -> where is this value on the board?
@@ -36,6 +53,7 @@ class Node:
         self.g = None
         self.h = None
         self.f = None
+        self.parent = None
 
     def __str__(self):
         return "Node: %s" % self.val
@@ -43,12 +61,23 @@ class Node:
     def __repr__(self):
         return self.__str__()
 
+    @property
+    def display_value(self):
+        if self.val == 0:
+            return " "
+
+        else:
+            return self.val
+
     def calc(self, g=True, h=False):
         """
         Heuristic will be the manhatten distance
 
         Can calculate the both the g and h costs with the associated flags
+        :param bool g:
+        :param bool h:
         :return:
+        :rtype: int
         """
         start = None
         end = None
@@ -74,6 +103,11 @@ class Node:
         return dst
 
     def calc_f(self):
+        """
+        Returns the sum of the g and h values for this node
+        :return:
+        :rtype: int
+        """
         return self.calc(g=True) + self.calc(h=True)
 
     def validate_goal_state(self):
@@ -81,12 +115,18 @@ class Node:
         Will make sure that value of the node aligns with its position
 
         In this case, the optimal puzzle value will have the empty space at pos 0
-        :return:
+        :return: True if in the goal state, otherwise False.
+        :rtype: bool
         """
         return self.val == self.pos
 
     def valid_movement_positions(self, position):
+        """
+        'A little janky'
 
+        :param int position:
+        :return:
+        """
         if position == 0:
             return (1, 3)
 
@@ -114,6 +154,7 @@ class Node:
         elif position == 8:
             return (5, 7)
 
+
 class Puzzle:
     """
     To represent one puzzle instance based on a sample file
@@ -134,7 +175,16 @@ class Puzzle:
         else:
             self.map = self.parse_puzzle_string()
 
+        self.graph = nx.DiGraph()
+
     def parse_file(self, file):
+        """
+        Parse the file
+
+        :param file:
+        :return:
+        :rtype: dict
+        """
         file = open(file)
         node_map = {}
         pos = 0
@@ -143,7 +193,6 @@ class Puzzle:
                 node = Node(int(val), pos)
                 node_map[pos] = node
                 pos += 1
-
 
         return node_map
 
@@ -159,19 +208,30 @@ class Puzzle:
 
         for pos, node in self.map.items():
             if cnt % 3 == 0:
-                print(node.val)
+                print(node.display_value)
             else:
-                print(node.val, end=" ")
+                print(node.display_value, end=" ")
 
             cnt +=1
 
     def get_start_node(self):
+        """
+        Get the starting node
+
+        :return: Start node
+        :rtype: Node
+        """
         for pos, node in self.map.items():
             if node.val == 0:
                 return node
 
-
     def neighbors(self, node):
+        """
+        Return the nodes that can be switched with a given node
+        :param node: The node to find the valid switches for
+        :return: dict of the format {node_position: node, node_position: node}
+        :rtype: dict
+        """
         valid_movement_positions = node.valid_movement_positions(node.pos)
         neighbor_nodes = {pos:_node for pos,_node in self.map.items() if pos in valid_movement_positions}
 
@@ -180,8 +240,8 @@ class Puzzle:
     def move_node(self, moving_node, empty_node):
         """
         Switches a real node with the node holding the val 0
-        :param moving_node: The node that is being moved into the "empty" space, which is just a node with val 0
-        :param empty_node:
+        :param Node moving_node: The node that is being moved into the "empty" space, which is just a node with val 0
+        :param Node empty_node:
         :return:
         """
         if empty_node.val != 0:
@@ -199,40 +259,62 @@ class Puzzle:
         self.map[empty_pos].pos = moving_pos
         self.map[moving_pos].pos = empty_pos
 
-
     def solve(self):
-        current_node = self.get_start_node()
+        """
+        You know, do solving things that don't actually work.
 
-        open = [current_node]
-        closed = []
+        :return:
+        """
+        start_node = self.get_start_node()
+
+        open = set()
+        open.add(start_node)
+        closed = set()
 
         # to speed this up in the future, we can keep a list of nodes that have reached goal state and check length
-        while True:
-            neighbors = self.neighbors(current_node)
-            neighbor_values = {}
+        while len(open) > 0:
+            self.print_puzzle()
+            current_node = min(open, key=lambda x: x.calc_f())
 
-            for pos, neighbor in neighbors.items():
-                neighbor_values[neighbor.calc_f()] = neighbor
+            open.remove(current_node)
+            closed.add(current_node)
 
-            lowest_f_node = neighbor_values[min(neighbor_values)]
-            current_node = neighbors[lowest_f_node.pos]
-
-            open.pop(current_node)
-            closed.append(current_node)
-
-            # is path found ??
             if self.validate_goal_state():
                 return
 
+            neighbors = self.neighbors(current_node)
 
+            for pos, neighbor in neighbors.items():
+                if neighbor in closed:
+                    continue
+
+                if neighbor.calc_f() < current_node.calc_f() or neighbor not in open:
+
+                    neighbor.parent = current_node
+                    if neighbor not in open:
+                        open.add(neighbor)
+
+            # neighbor_values = {}
+            # for pos, neighbor in neighbors.items():
+            #     neighbor_values[neighbor.calc_f()] = neighbor
+            #
+            # lowest_f_node = neighbor_values[min(neighbor_values)]
+            # current_node = neighbors[lowest_f_node.pos]
+            # is path found ??
+            # if self.validate_goal_state():
+            #     return
 
     def validate_goal_state(self):
+        """
+        Validate whether all nodes are in their proper place
+        :return:
+        :rtype: bool
+        """
         for pos, node in self.map.items():
             if not node.validate_goal_state():
                 return False
 
-        return False
-
+        return True
 
 
 p1 = Puzzle('sample-problems/p1', True)
