b7e03c03b24c67d555ab6e1e06cc509075856044
carvey <j95.charles@gmail.com>
2016-10-30 23:42:57 -0400

    Touched up the state logic and started messing with adding puzzle states to the graph

--- a_star.py
+++ a_star.py
@@ -8,19 +8,23 @@ Goal State:
 3 4 5
 6 7 8
 """
+import matplotlib
 import networkx as nx
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 # import pylab
 
 # # Initialize the graph
-# graph = networkx.DiGraph()
+# graph = nx.DiGraph()
 #
 # # Add edges
 # for edge in Main.__graph.edge_set:
 #     graph.add_edge(edge.node_from.name, edge.node_to.name)
 #
 # # Set layout, draw the graph, and display it
-# pos = networkx.shell_layout(graph)
-# networkx.draw(graph, pos, node_size=1500, node_color='yellow', edge_color='red', with_labels=True)
+# pos = nx.shell_layout(graph)
+# nx.draw(graph, pos, node_size=1500, node_color='yellow', edge_color='red', with_labels=True)
 # # Show the graph
 # # pylab.show()
 
@@ -167,15 +171,37 @@ class Puzzle:
         :param file: Flag to set if passing in file
         :return:
         """
-        self.map = None
+        start_state = None
+        goal_state = None
 
-        if file:
-            self.map = self.parse_file(data)
+        if file: # the data being passed in is a file to be parsed
+            start_state, goal_state = self.parse_file(data)
 
-        else:
-            self.map = self.parse_puzzle_string()
+        else: # the data being passed in is a string containing a start and goal state
+            start_state, goal_state = self.parse_full_data_string(data)
+
+        # self.state = PuzzleState(state)
+        self.start_state = PuzzleState(start_state)
+        self.goal_state = PuzzleState(goal_state)
 
         self.graph = nx.DiGraph()
+
+    def parse_data(self, data):
+        """
+
+        :param data: string containing just one string state -> start state or goal state
+        :return:
+        """
+        pos = 0
+        state_map = {}
+
+        data = data.replace(" ", "").replace("\n", "")
+        for val in data:
+            node = Node(int(val), pos)
+            state_map[pos] = node
+            pos += 1
+
+        return state_map
 
     def parse_file(self, file):
         """
@@ -186,33 +212,104 @@ class Puzzle:
         :rtype: dict
         """
         file = open(file)
-        node_map = {}
-        pos = 0
-        for val in file.read():
-            if val.strip("\n").isalnum():
-                node = Node(int(val), pos)
-                node_map[pos] = node
-                pos += 1
+        data = file.read() # get the entire contents of the file
 
-        return node_map
+        return self.parse_full_data_string(data) # parse both the start and initial state
 
-    def parse_puzzle_string(self):
+
+    def parse_full_data_string(self, full_data_string):
         """
-        Read puzzle data and create a node tree from a string
+        :param full_data_string: a string containing the start state and goal state, deliminated with two newline chars
         :return:
         """
-        pass
+        data = full_data_string.split("\n\n")
 
-    def print_puzzle(self):
-        cnt = 1
+        start_data = data[0]
+        goal_data = data[1]
 
-        for pos, node in self.map.items():
-            if cnt % 3 == 0:
-                print(node.display_value)
-            else:
-                print(node.display_value, end=" ")
+        start_map = self.parse_data(start_data)
+        goal_map = self.parse_data(goal_data)
 
-            cnt +=1
+        return start_map, goal_map
+
+    def solve(self):
+        """
+        You know, do solving things that don't actually work.
+
+        :return:
+        """
+        start_state = self.start_state
+        self.graph.add_node(start_state)
+
+        # find neighbors (expand current state, finding all possible node moves) and calculate f costs
+        start_node = start_state.get_start_node()
+        for pos, neighbor in start_state.neighbors(start_node).items():
+            copied_state = start_state.move_node(neighbor, start_node)
+            puzzle_state = PuzzleState(copied_state)
+
+            self.graph.add_edge(start_node, puzzle_state)
+
+
+        # open = set()
+        # open.add(start_node)
+        # closed = set()
+        #
+        # # to speed this up in the future, we can keep a list of nodes that have reached goal state and check length
+        # while len(open) > 0:
+        #     self.print_puzzle()
+        #     current_node = min(open, key=lambda x: x.calc_f())
+        #
+        #     open.remove(current_node)
+        #     closed.add(current_node)
+        #
+        #     if self.validate_goal_state():
+        #         return
+        #
+        #     neighbors = self.neighbors(current_node)
+        #
+        #     for pos, neighbor in neighbors.items():
+        #         if neighbor in closed:
+        #             continue
+        #
+        #         if neighbor.calc_f() < current_node.calc_f() or neighbor not in open:
+        #
+        #             neighbor.parent = current_node
+        #             if neighbor not in open:
+        #                 open.add(neighbor)
+
+        # neighbor_values = {}
+        # for pos, neighbor in neighbors.items():
+        #     neighbor_values[neighbor.calc_f()] = neighbor
+        #
+        # lowest_f_node = neighbor_values[min(neighbor_values)]
+        # current_node = neighbors[lowest_f_node.pos]
+        # is path found ??
+        # if self.validate_goal_state():
+        #     return
+
+class PuzzleState:
+
+    def __init__(self, state):
+
+        self.state = state
+
+    def __str__(self):
+        return self.print_state()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def validate_goal_state(self):
+        """
+        Validate whether all nodes are in their proper place
+        :return:
+        :rtype: bool
+        """
+        for pos, node in self.state.items():
+            if not node.validate_goal_state():
+                return False
+
+        return True
 
     def get_start_node(self):
         """
@@ -221,21 +318,9 @@ class Puzzle:
         :return: Start node
         :rtype: Node
         """
-        for pos, node in self.map.items():
+        for pos, node in self.state.items():
             if node.val == 0:
                 return node
-
-    def neighbors(self, node):
-        """
-        Return the nodes that can be switched with a given node
-        :param node: The node to find the valid switches for
-        :return: dict of the format {node_position: node, node_position: node}
-        :rtype: dict
-        """
-        valid_movement_positions = node.valid_movement_positions(node.pos)
-        neighbor_nodes = {pos:_node for pos,_node in self.map.items() if pos in valid_movement_positions}
-
-        return neighbor_nodes
 
     def move_node(self, moving_node, empty_node):
         """
@@ -252,71 +337,45 @@ class Puzzle:
         empty_pos = empty_node.pos
 
         # switch the nodes in Puzzles position, val map
-        self.map[empty_pos] = moving_node
-        self.map[moving_pos] = empty_node
+        self.state[empty_pos] = moving_node
+        self.state[moving_pos] = empty_node
 
         # switch the position on the nodes themselves
-        self.map[empty_pos].pos = moving_pos
-        self.map[moving_pos].pos = empty_pos
+        self.state[empty_pos].pos = moving_pos
+        self.state[moving_pos].pos = empty_pos
 
-    def solve(self):
+    def print_state(self):
         """
-        You know, do solving things that don't actually work.
 
+        :param PuzzleState puzzle_state:
         :return:
         """
-        start_node = self.get_start_node()
+        cnt = 1
 
-        open = set()
-        open.add(start_node)
-        closed = set()
+        for pos, node in self.state.items():
+            if cnt % 3 == 0:
+                print(node.display_value)
+            else:
+                print(node.display_value, end=" ")
 
-        # to speed this up in the future, we can keep a list of nodes that have reached goal state and check length
-        while len(open) > 0:
-            self.print_puzzle()
-            current_node = min(open, key=lambda x: x.calc_f())
+            cnt +=1
 
-            open.remove(current_node)
-            closed.add(current_node)
-
-            if self.validate_goal_state():
-                return
-
-            neighbors = self.neighbors(current_node)
-
-            for pos, neighbor in neighbors.items():
-                if neighbor in closed:
-                    continue
-
-                if neighbor.calc_f() < current_node.calc_f() or neighbor not in open:
-
-                    neighbor.parent = current_node
-                    if neighbor not in open:
-                        open.add(neighbor)
-
-            # neighbor_values = {}
-            # for pos, neighbor in neighbors.items():
-            #     neighbor_values[neighbor.calc_f()] = neighbor
-            #
-            # lowest_f_node = neighbor_values[min(neighbor_values)]
-            # current_node = neighbors[lowest_f_node.pos]
-            # is path found ??
-            # if self.validate_goal_state():
-            #     return
-
-    def validate_goal_state(self):
+    def neighbors(self, node):
         """
-        Validate whether all nodes are in their proper place
-        :return:
-        :rtype: bool
+        Return the nodes that can be switched with a given node
+        :param node: The node to find the valid switches for
+        :return: dict of the format {node_position: node, node_position: node}
+        :rtype: dict
         """
-        for pos, node in self.map.items():
-            if not node.validate_goal_state():
-                return False
+        valid_movement_positions = node.valid_movement_positions(node.pos)
+        neighbor_nodes = {pos:_node for pos,_node in self.state.items() if pos in valid_movement_positions}
 
-        return True
+        return neighbor_nodes
 
 
 p1 = Puzzle('sample-problems/p1', True)
-p1.print_puzzle()
+p1.start_state.print_state()
 p1.solve()
+
+nx.draw(p1.graph)
+plt.show()
